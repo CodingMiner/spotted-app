550ec840c83085edc663c3533eb11150ff8d11e8
Karol Górnik <155629482+CodingMiner@users.noreply.github.com>
2026-03-10 14:02:46 +0100

    Refactor AuthorizationButton component to use MUI styling and improve button design

--- src/components/Button.ts
+++ src/components/Button.ts
@@ -1,25 +1,26 @@
-import Button from "@material-ui/core/Button";
-import { withStyles } from "@material-ui/core/styles";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
 
-export const AuthorizationButton = withStyles({
-  root: {
+export const AuthorizationButton = styled(Button)({
+  boxShadow: "none",
+  textTransform: "none",
+  fontSize: 20,
+  fontWeight: 800,
+  letterSpacing: "1px",
+  color: "#ffffff",
+  padding: "12px 56px",
+  lineHeight: 1.5,
+  backgroundColor: "#2ebc58",
+  borderColor: "#2ebc58",
+  borderRadius: 30,
+  "&:hover": {
+    backgroundColor: "#2E8B57",
+    borderColor: "#2E8B57",
     boxShadow: "none",
-    textTransform: "none",
-    fontSize: 20,
-    padding: "10px 50px",
-    lineHeight: 1.5,
-    backgroundColor: "#2ebc58",
-    borderColor: "#2ebc58",
-    borderRadius: 30,
-    "&:hover": {
-      backgroundColor: "#2E8B57",
-      borderColor: "#2E8B57",
-      boxShadow: "none",
-    },
-    "&:active": {
-      backgroundColor: "#2E8B57",
-      borderColor: "#2E8B57",
-      boxShadow: "none",
-    },
   },
-})(Button);
+  "&:active": {
+    backgroundColor: "#2E8B57",
+    borderColor: "#2E8B57",
+    boxShadow: "none",
+  },
+});
